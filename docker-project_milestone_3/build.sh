a6531c05b2e7f66385aac5a32d0c3619c9a64952
vqnguyen86 <113203721+vqnguyen86@users.noreply.github.com>
2022-12-20 12:13:32 -0500

    Update build.sh

--- docker-project_milestone_3/build.sh
+++ docker-project_milestone_3/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "TODO: fill in the docker build command"
-docker build -t ift6758/serving:1.0.0 . -f Dockerfile.serving
+docker build -t ift6758/serving:0.0.1 . -f Dockerfile.serving
 
-docker build -t ift6758/serving:2.0.0 . -f Dockerfile.streamlit
+docker build -t ift6758/serving:0.0.2 . -f Dockerfile.streamlit
 
